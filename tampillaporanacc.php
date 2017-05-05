1c292feb3a45b3261046c1c605b690a4f050abc3
Andree Sulistya <andreesulistyasaputra@gmail.com>
2017-05-05 21:01:06 +0700

    update lagi

---
+++ tampillaporanacc.php
@@ -0,0 +1,60 @@
+<?php include ('headeradmin.php'); ?>
+<body>
+<div class="agileinfo-work-top">
+		<div class="container">
+			<div class="w3-agileits-rides-heading">
+				<h3 class="w3ls_head">Our Recent <span>Works</span></h3>
+			</div>
+			<table class="table">
+                <thead>
+                <tr>
+                  <th>ID Laporan</th>
+                  <th>Judul Laporan</th>
+                  <th>Jenis Laporan</th>
+                  <th>Nama Pelapor</th>
+                  <th>Nomor Pelapor</th>
+                  <th>Lokasi</th>
+                  <th>Isi Laporan</th>
+                  <th>Gambar</th>
+                  <th>Keterangan</th>
+                  <th>Aksi</th>
+                </tr>
+                </thead>
+                <tbody>
+                  <?php
+                  if (empty($laporan))
+                  {
+                    echo "<tr><td colspan=\"9\">Data tidak tersedia</td></tr>";
+                  }else
+                  {
+                     foreach ($laporan as $isi)
+                  {
+                  ?>
+                     <tr height=20>
+                             
+                            <td><?php print $isi->idlaporan?> </td>
+                            <td ><?php print $isi->judullaporan?> </td>
+                            <td ><?php print $isi->jenislaporan?></td>
+                            <td ><?php print $isi->namapelapor?></td>
+                            <td ><?php print $isi->nomorpelapor?></td>
+                            <td><?php print $isi->lokasi?></td>
+                            <td ><?php print $isi->isilaporan?></td>
+                            <td ><?php print $isi->gambar?></td>
+                            <td ><?php print $isi->keterangan?></td>
+                            <td>
+                            <?php echo anchor('laporan/terima_laporan/'.$isi->idlaporan, 'TERIMA')."|"?> 
+                            <?php echo anchor('laporan/konfirm_hapus/'.$isi->idlaporan, 'TIDAK')?>
+                            </td>
+                            
+                     </tr>
+                     
+                     <?php
+                   		
+                   }}
+                   ?>
+                </tbody>
+                
+              </table>
+		</div>
+	</div>
+	</body>
