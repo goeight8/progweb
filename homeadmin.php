7587d0a8f0c48a2fee60c65eafa632fd9678376b
fadhelnuh <fadhelnuh55@gmail.com>
2017-05-05 20:58:50 +0700

    update home admin

---
+++ homeadmin.php
@@ -0,0 +1,30 @@
+
+<body>
+<!-- header -->
+	<?php include ('headeradmin.php');?>
+	<!-- //header -->
+	<!-- top-header and slider -->
+	
+	<!-- //top-header and slider -->
+	<!-- Specialize-section -->
+			<section class="w3-about text-center">
+				<div class="container">
+					<h2 class="w3ls_head">SELAMAT DATANG<br><?php echo $this->session->userdata('namaadmin'); ?></h2>
+					
+					<div class="clearfix"></div>
+				</div>
+			</section>
+	<!-- //Specialize-section -->
+	<!--clients-->
+			
+	<!--//clients-->
+		<!--advantage-->
+
+			<!--advantage-->
+	<!--stats-->
+
+<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+<script src="<?php echo base_url();?>asset/js/bootstrap.js"></script>
+<script type="text/javascript" src="<?php echo base_url();?>asset/js/numscroller-1.0.js"></script>
+</body>
+</html>
